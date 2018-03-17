790db730ef7470194679386d2d651425ae94dada
reeeexchen <rexchen1997@yahoo.com>
2018-03-18 00:55:44 +0800

    UPDATE     VERSION 0.0.1:     SUI MOBILE

---
+++ src/main/java/com/imooc/o2o/web/shopadmin/ShopAdminController.java
@@ -0,0 +1,20 @@
+package com.imooc.o2o.web.shopadmin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * 店铺页面转发 路由规则
+ * @Author:REX
+ * @Date: Create in 0:38 2018/3/18
+ */
+@Controller
+@RequestMapping(value = "shopadmin",method = {RequestMethod.GET})
+public class ShopAdminController {
+
+	@RequestMapping(value = "/shopoperation")
+	public String shopOperation(){
+		return "shop/shopoperation";
+	}
+}
--- src/main/java/com/imooc/o2o/web/shopadmin/ShopManagementController.java
+++ src/main/java/com/imooc/o2o/web/shopadmin/ShopManagementController.java
@@ -1,10 +1,14 @@
 package com.imooc.o2o.web.shopadmin;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.imooc.o2o.entity.Shop;
 import com.imooc.o2o.util.HttpServletRequestUtil;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
@@ -24,8 +28,21 @@ public class ShopManagementController {
 		Map<String,Object> modelMap = new HashMap<String, Object>();
 		//1.接受并转化相应的参数,包括店铺信息以及图片信息
 		String shopStr = HttpServletRequestUtil.getString(request,"shopStr");
-
+		//<!-- json解析-->
+		ObjectMapper mapper = new ObjectMapper(); // create once, reuse
+		Shop shop = null;
+		try{
+			shop = mapper.readValue(shopStr,Shop.class);
+		}catch (Exception e){
+			modelMap.put("success",false);
+			modelMap.put("errMsg",e.getMessage());
+			return modelMap;
+		}
+		CommonsMultipartFile shopImg = null;
+		CommonsMultipartResolver commonsMultipartResolver = new CommonsMultipartResolver();
 		//2.注册店铺
 		//3.返回结果
+
+		return null;
 	}
 }
